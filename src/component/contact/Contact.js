87cab1b3f01e4c900f1b51525a373aab0a3ac145
Akshay <helloakshay27@gmail.com>
2024-01-30 12:01:31 +0530

    Contact module push

--- src/component/contact/Contact.js
+++ src/component/contact/Contact.js
@@ -4,20 +4,20 @@ import "./Contact.css";
 const Contact = () => {
   return (
     <section className="contactpage mb-3 mt-4 ">
-    <div className=" container">
-      <h2 className="contact_headline my-3">
-        Ready to accelerate your business?{" "}
-        <span className="talk_option"> Let's talk.</span>
+    <div className=" container-fluid px-0">
+      <h2 className="contact_headline ">
+        <span data-aos="fade-up">Ready to accelerate your business?{" "}</span>
+        <span data-aos="fade-up" className="talk_option"> Let's talk.</span>
       </h2>
       <div className="contact_container my-3">
-        <h2>
-          Keep track of the wolves
-          <p className="arrow">&rarr; </p>
+        <h2 >
+         <span data-aos="fade-up">Keep track of the wolves</span> 
+          <p data-aos="slide-right" className="arrow">&rarr; </p>
         </h2>
         <div className="contact_image">
-          <img src="./Assets/contactimg1.png" alt="contactimg" />
-          <img src="./Assets/contactimg2.png" alt="contactimg" />
-          <img src="./Assets/contactimg3.png" alt="contactimg" />
+          <img data-aos="flip-left" src="./Assets/contactimg1.png" alt="contactimg" />
+          <img data-aos="fade-up" src="./Assets/contactimg2.png" alt="contactimg" />
+          <img data-aos="flip-right" src="./Assets/contactimg3.png" alt="contactimg" />
         </div>
       </div>
     </div>
