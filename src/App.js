0f6ab6d50886b96a570099289311d4d44ab408aa
Akshay <helloakshay27@gmail.com>
2024-01-30 12:00:05 +0530

    Add aos animation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import "./App.css";
 import Navbar from "./component/navbar/Navbar.js";
 import Home from "./component/home/Home.js";
@@ -5,8 +6,18 @@ import Client from "./component/client/Client.js";
 import Contact from "./component/contact/Contact.js";
 import FooterComp from "./component/footerComp/FooterComp.js";
 import About from "./component/about/About.js";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Specify the duration of the animation in milliseconds
+      easing: 'ease-in-out', // Specify the easing function
+    });
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
